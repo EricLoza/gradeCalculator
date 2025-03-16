d2b996fbaf287bd8d170e4d10433c32bde0f4bef
Eric Loza <96986695+EricLoza@users.noreply.github.com>
2025-03-16 14:19:20 -0400

    Fixed random error bug

--- calculations.cpp
+++ calculations.cpp
@@ -1,34 +1,31 @@
 #include "header.hpp"
-class calculateGrade {
+
+void calculateGrade::percentage( map<string, double>& gradePercents) {
+
+    for(auto &percent : gradePercents) {
+        cout << "Enter the weight percentage for " << percent.first << ": ";
+        cin >> percent.second; 
     
-    public: 
-    void percentage( map<string, double>& gradePercents) {
-
-        for(auto &percent : gradePercents) {
-            cout << "Enter the weight percentage for " << percent.first << ": ";
-            cin >> percent.second; 
-        
-            }
-        
-            for(auto &percent : gradePercents) { 
-            cout << percent.first << " " << percent.second << endl; 
-            }
-            cout << endl;
-    }
-
-    void grade(map<string, double>& gradeValue) {
-        for(auto &value : gradeValue) {
-            cout << "Enter the weight percentage for " << value.first << ": ";
-            cin >> value.second; 
-        
         }
-        
-        for(auto &value : gradeValue) { 
-            cout << value.first << " " << value.second << endl; 
-        }       
-    }
+    
+        for(auto &percent : gradePercents) { 
+        cout << percent.first << " " << percent.second << endl; 
+        }
+        cout << endl;
+}
 
-    double calculatePercentage(map<string, double>& gradePercents, map<string, double>& gradeValue) {
-
+void calculateGrade::grade(map<string, double>& gradeValue) {
+    for(auto &value : gradeValue) {
+        cout << "Enter the weight percentage for " << value.first << ": ";
+        cin >> value.second; 
+    
     }
-};
+    
+    for(auto &value : gradeValue) { 
+        cout << value.first << " " << value.second << endl; 
+    }       
+}
+
+// double calculatePercentage(map<string, double>& gradePercents, map<string, double>& gradeValue) {
+
+// }
