622b9123501ee5fcf84386c0dd26de7ac5312f4e
Eric Loza <ericlozza@gmail.com>
2025-03-16 11:48:29 -0400

    This is a test

---
+++ calculations.cpp
@@ -0,0 +1,22 @@
+#include "header.hpp"
+
+
+class calculateGrade {
+
+    public: 
+    void percentage() {
+
+        double assignmentPercent, participationPercent, discussionsPercent, examPercent, testPercent, projectPercent, attendancePercent; 
+
+    }
+
+    public: 
+    void grade() {
+        double assignmentGrade, participationGrade, discussionsGrade, examsGrade, testGrade, projectGrade, attendanceGrade; 
+    }
+
+    public:
+    double calculatePercentage(double assignmentPercent, double participationPercent, double discussionsPercent, double examPercent, double testPercent, double projectPercent, double attendancePercent) {
+
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include "header.hpp"
+/*A C++ project is designed with an object-oriented approach, with classes for customers, accounts, transactions, and banking services. Features included creating and managing customers, accounts, and transactions, as well as performing banking services such as withdrawals, deposits, and transfers. It also allows customers to view their account information, including account balances, recent transactions, and other details.
+
+Technologies Required: C++ programming language, OOPS, and MySQL.*/
+using namespace std;
+
+int main() { 
+    double assignmentPercent, participationPercent, discussionsPercent, examPercent, 
+    testPercent, projectPercent, attendancePercent;
+
+    double assignmentGrade, participationGrade, 
+    discussionsGrade, examsGrade, testGrade, projectGrade, attendanceGrade; 
+
+    cout << "test";
+    map<string, double> gradeWeights = {
+        {"Assignment Percent", assignmentPercent},
+        {"Participation Percent", participationPercent},
+        {"Discussion Percent", discussionsPercent},
+        {"Exam Percent", examPercent},
+        {"Test Percent", testPercent},
+        {"Project Percent", projectPercent},
+        {"Attendance Percent", attendancePercent}
+    };
+
+    map<string, double> gradeValue = {
+        {"Assignment Grade", assignmentGrade},
+        {"Participation Grade", participationGrade},
+        {"Discussion Grade", discussionsGrade},
+        {"Exam Grade", examsGrade},
+        {"Test Grade", testGrade},
+        {"Project Grade", projectGrade},
+        {"Attendance Grade", attendanceGrade}
+     };
+    cout << "Let's calculate your grade!\n";
+
+  for(auto &weight : gradeWeights) {
+    cout << "Enter the weight percentage for " << weight.first << ": ";
+    cin >> weight.second; 
+
+  }
+
+  for(auto &weight : gradeWeights) { 
+    cout << weight.first << " " << weight.second << endl; 
+  }
+  cout << endl;
+  for(auto &value : gradeValue) {
+    cout << "Enter the weight percentage for " << value.first << ": ";
+    cin >> value.second; 
+
+  }
+
+  for(auto &value : gradeValue) { 
+    cout << value.first << " " << value.second << endl; 
+  }
+  cout << endl;
+  cout << endl;
+  cout << attendanceGrade;
+}
