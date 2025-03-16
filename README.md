8fc851afff3faa19e2ff734985316625f541d310
Eric Loza <96986695+EricLoza@users.noreply.github.com>
2025-03-16 18:03:45 -0400

    It is done

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-NOTE: This is a work-in-progress code. It does not work as intended at the time of writing this. I am working on this in my free time. 
+It is done! 
--- main.cpp
+++ main.cpp
@@ -2,30 +2,24 @@
 
 int main() { 
 
-  double assignmentPercent, participationPercent, discussionsPercent, examPercent, 
-  testPercent, projectPercent, attendancePercent;
-
-  double assignmentGrade, participationGrade, discussionsGrade, examsGrade, testGrade, projectGrade, attendanceGrade;
-
-
   map<string, double> gradePercents = {
-    {"Assignment Percent", assignmentPercent = 0.0},
-    {"Participation Percent", participationPercent = 0.0},
-    {"Discussion Percent", discussionsPercent = 0.0},
-    {"Exam Percent", examPercent = 0.0},
-    {"Test Percent", testPercent = 0.0},
-    {"Project Percent", projectPercent = 0.0},
-    {"Attendance Percent", attendancePercent = 0.0}
+    {"Assignment", 0.0},
+    {"Participation", 0.0},
+    {"Discussion", 0.0},
+    {"Exam", 0.0},
+    {"Test", 0.0},
+    {"Project", 0.0},
+    {"Attendance", 0.0}
   };
 
   map<string, double> gradeValue = {
-    {"Assignment Grade", assignmentGrade = 0.0},
-    {"Participation Grade", participationGrade = 0.0},
-    {"Discussion Grade", discussionsGrade = 0.0},
-    {"Exam Grade", examsGrade = 0.0},
-    {"Test Grade", testGrade = 0.0},
-    {"Project Grade", projectGrade = 0.0},
-    {"Attendance Grade", attendanceGrade = 0.0}
+    {"Assignment",  0.0},
+    {"Participation", 0.0},
+    {"Discussion", 0.0},
+    {"Exam", 0.0},
+    {"Test", 0.0},
+    {"Project", 0.0},
+    {"Attendance", 0.0}
   };
 
   cout << "Let's calculate your grade!\n";
